85e1566d8ad560f5591f643c686aec044e3e44b0
EmirSab <emir.sabanic.ro@gmail.com>
2019-01-06 21:52:23 +0100

    Full Name added on login

---
+++ EmirApp/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace EmirApp.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EmirApp.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "EmirApp.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(EmirApp.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
