b2033d13acadb27c18d6cfd8175232345a5747cc
EmirSab <emir.sabanic.ro@gmail.com>
2019-02-02 17:10:29 +0100

    Delete user

---
+++ EmirApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using EmirApp.DAL;
+using EmirApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmirApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+            return View(data.ToList());
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.... It will show only to authorized person";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EmirApp/Migrations/201901182040527_column.cs
@@ -0,0 +1,18 @@
+namespace EmirApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class column : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Student", name: "FirstMidName", newName: "FirstName");
+        }
+        
+        public override void Down()
+        {
+            RenameColumn(table: "dbo.Student", name: "FirstName", newName: "FirstMidName");
+        }
+    }
+}
